4de506e37670773a2df0948e35f99098b98ec361
Isaac <isaachkdeveloper@gmail.com>
2023-11-06 20:10:28 +0800

    feat: update chat engine

--- app/main.py
+++ app/main.py
@@ -11,6 +11,20 @@ from llama_index import StorageContext, ServiceContext, load_index_from_storage,
 from llama_index.llms import Replicate
 from langchain.embeddings import CohereEmbeddings
 
+class Message(BaseModel):
+    role: str
+    content: str
+
+
+class ChatData(BaseModel):
+    messages: List[Message]
+    id: str
+    previewToken: Optional[str] = None
+
+
+class TestData(BaseModel):
+    prompt: str
+
 app = FastAPI()
 
 load_dotenv()
@@ -55,7 +69,7 @@ async def generate(prompt: str):
         yield item
 
 
-async def ask(prompt: str):
+async def ask(prompt: str, messages: List[Message]):
     llama2 = "meta/llama-2-13b-chat:f4e2de70d66816a838a89eeeb621910adffb0dd0baba3976c96980970978018d"
     llm = Replicate(
         model=llama2,
@@ -68,43 +82,20 @@ async def ask(prompt: str):
 
     storage_context = StorageContext.from_defaults(persist_dir='./storage')
     index = load_index_from_storage(storage_context, service_context=service_context)
-    # query_engine = index.as_query_engine(streaming=True, similarity_top_k=1)
-    # custom_chat_history = []
-    # chat_engine = CondenseQuestionChatEngine.from_defaults(
-    #     query_engine=query_engine, 
-    #     condense_question_prompt=custom_prompt,
-    #     chat_history=custom_chat_history,
-    #     service_context=service_context,
-    #     verbose=True,
-    # )
 
     with open("./data/system_prompt.txt", "r") as file:
         system_prompt = file.read()    
 
     chat_engine = index.as_chat_engine(
         chat_mode="context",
-        system_prompt=system_prompt
-        ,
+        system_prompt=system_prompt,
+        chat_history=messages,
     )
 
     streaming_response = chat_engine.stream_chat(message=prompt)
     for text in streaming_response.response_gen:
         yield text
 
-
-class Message(BaseModel):
-    role: str
-    content: str
-
-
-class ChatData(BaseModel):
-    messages: List[Message]
-    id: str
-    previewToken: Optional[str] = None
-
-
-class TestData(BaseModel):
-    prompt: str
 
 
 @app.get("/")
@@ -119,7 +110,7 @@ def chat(data: ChatData):
         return StreamingResponse(introduce())
 
     message = data.messages[-1].content
-    return StreamingResponse(ask(message))
+    return StreamingResponse(ask(message, data.messages))
     # return StreamingResponse(generate(message))
 
 
